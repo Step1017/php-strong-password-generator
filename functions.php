65af43157cbd1a33a15d0b363f0a428c704fc767
Step1017 <s.aveta@yahoo.com>
2023-02-22 18:08:32 +0100

    Mod val da 65 a 48 per aggiungere numeri a pwd

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
     function generatePassword ($num) {
         $password="";
         for ($i=1; $i<=$num; $i++){
-            $password .= chr(rand(65, 122));
+            $password .= chr(rand(48, 122));
             echo "<script>console.log('{$password}')</script>";
         };
         return $password;
